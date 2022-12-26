8c1de1f567fcc62737a4fdad50655aa1c2dd9c89
한동진 <arkc1009@gmail.com>
2022-12-26 16:54:10 +0900

    chore: set local font and color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import LocalFont from '@next/font/local';
+
+const NanumNeo = LocalFont({
+  src: '../asset/Nanum/NanumSquareNeo-Variable.woff',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={NanumNeo.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
