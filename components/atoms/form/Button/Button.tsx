2e2446429107d20024b0aa94765a14639646e248
한동진 <arkc1009@gmail.com>
2022-12-27 15:32:20 +0900

    feat: add Button Component

---
+++ components/atoms/form/Button/Button.tsx
@@ -0,0 +1,27 @@
+import { ComponentProps, useMemo } from 'react';
+import styles from './Button.module.css';
+
+type ButtonKind =
+  | 'primary'
+  | 'secondery'
+  | 'ghost'
+  | 'text'
+  | 'google'
+  | 'kakao'
+  | 'naver';
+
+interface ButtonProps extends ComponentProps<'button'> {
+  kind?: ButtonKind;
+}
+
+function Button({ kind = 'primary', children, ...rest }: ButtonProps) {
+  const buttonKindStyles = useMemo(() => styles[kind], [kind]);
+
+  return (
+    <button className={`${styles.base} ${buttonKindStyles}`} {...rest}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
