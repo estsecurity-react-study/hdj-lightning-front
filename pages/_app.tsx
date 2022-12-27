d3cfd5da46104d28d615a71e5a11966c87247be6
한동진 <arkc1009@gmail.com>
2022-12-27 15:31:31 +0900

    feat: apply style1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,13 @@ import type { AppProps } from 'next/app';
 import LocalFont from '@next/font/local';
 
 const NanumNeo = LocalFont({
-  src: '../asset/Nanum/NanumSquareNeo-Variable.woff',
+  src: '../public/asset/font/Nanum/NanumSquareNeo-Variable.woff',
+  variable: '--nanum-font',
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={NanumNeo.className}>
+    <main className={`${NanumNeo.variable}`}>
       <Component {...pageProps} />
     </main>
   );
