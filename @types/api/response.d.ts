e90876501bf737623164022926e9c210e1b04fe3
ArKc1009 <arkc1009@gmail.com>
2023-01-06 22:24:57 +0900

    feat: add Provier Column in UserProfile type

--- @types/api/response.d.ts
+++ @types/api/response.d.ts
@@ -1,8 +1,16 @@
+export enum Provider {
+  local = 'local',
+  google = 'google',
+  kakao = 'kakao',
+  naver = 'naver',
+}
+
 export interface UserProfile {
   id: number;
   email: string;
   username: string;
   photo: string;
+  provider: Provider;
   createAt: string;
   updateAt: string;
 }
